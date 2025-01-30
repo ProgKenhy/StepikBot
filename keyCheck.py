de73917f3899746e1afeed0073a3ef839a911720
ProgKenhy <sashok20053@gmail.com>
2025-01-31 01:45:32 +0200

    Update keyCheck.py

--- keyCheck.py
+++ keyCheck.py
@@ -1,6 +1,7 @@
 from aiogram import Bot, Dispatcher, F
 from aiogram.filters import CommandStart
-from aiogram.types import KeyboardButton, Message, ReplyKeyboardMarkup
+from aiogram.types import KeyboardButton, Message, KeyboardButtonPollType, ReplyKeyboardMarkup, WebAppInfo
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
 from environs import Env
 
 env = Env()
@@ -11,34 +12,40 @@ BOT_TOKEN = env.str("BOT_TOKEN")
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
-buttons: list[list[KeyboardButton]] = [[KeyboardButton(
-    text=f'Button {j * 3 + i}') for i in range(1, 4)] for j in range(3)]
+kb_builder = ReplyKeyboardBuilder()
 
-keyboard = ReplyKeyboardMarkup(keyboard=buttons, one_time_keyboard=True, resize_keyboard=True)
+contact_btn = KeyboardButton(
+    text='Send phone number',
+    request_contact=True
+)
+
+geo_btn = KeyboardButton(
+    text='Send geolocation',
+    request_location=True
+)
+
+poll_btn = KeyboardButton(
+    text='Send poll',
+    request_poll=KeyboardButtonPollType()
+)
+
+web_app_btn = KeyboardButton(
+    text='Start web app',
+    web_app=WebAppInfo(url="https://stepik.org/")
+)
+
+kb_builder.row(contact_btn, geo_btn, poll_btn, web_app_btn, width=1)
+
+keyboard: ReplyKeyboardMarkup = kb_builder.as_markup(
+    resize_keyboard=True,
+    one_time_keyboard=True
+)
 
 
 @dp.message(CommandStart())
-async def process_start_command(message: Message):
-    await message.answer(
-        text='Чего кошки боятся больше?',
-        reply_markup=keyboard
-    )
-
-
-@dp.message(F.text == 'Dogs')
-async def process_dog_answer(message: Message):
-    await message.answer(
-        text='Да, несомненно, кошки боятся собак. '
-             'Но вы видели как они боятся огурцов?',
-    )
-
-
-@dp.message(F.text == 'cucumbers')
-async def process_cucumber_answer(message: Message):
-    await message.answer(
-        text='Да, иногда кажется, что огурцов '
-             'кошки боятся больше',
-    )
+async def command_start(message: Message):
+    await message.answer("experiment",
+                         reply_markup=keyboard)
 
 
 if __name__ == '__main__':
